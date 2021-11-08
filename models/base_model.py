1226c44b0072e5311b589ad70a74e468a783cfcd
Ayrton Coelho <3282@holbertonschool.com>
2021-11-08 13:33:05 -0300

    class ModelBase: first changes

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+'''
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    ''''''
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None and len(kwargs) >= 1:
+            for key, val in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f'))
+                    continue
+                setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj = dict(self.__dict__)
+        obj['__class__'] = type(self).__name__
+        obj['created_at'] = self.created_at.isoformat()
+        obj['updated_at'] = self.updated_at.isoformat()
+        return obj
+    
+    def __str__(self):
+        p = f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return p
