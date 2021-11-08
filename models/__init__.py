1f445c2a2b2a29bb1d4663bf64e737f40d31f043
Sebastian Moreira <3292@holbertonschool.com>
2021-11-08 15:28:08 -0300

    commit

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''
+'''
+from models.engine import file_storage as f_s
+
+storage = f_s.FileStorage()
+storage.reload()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''
+'''
+import json
+
+from models.base_model import BaseModel
+
+class FileStorage:
+    ''' File Storage '''
+    def __init__(self):
+        ''' init '''
+        self.__file_path = "file.json"
+        self.__objects = {}
+    
+    def all(self):
+        ''' returns the dictionary __objects '''
+        return self.__objects
+
+    def new(self, obj):
+        ''' sets in __objects the obj with key <obj class name>.id '''
+        key = str(obj.__class__) + "." + str(obj.id)
+        self.__objects[key] = obj
+    
+    def save(self):
+        ''' serializes __objects to the JSON file '''
+        obj_dict = {}
+        for key, value in self.__objects.items():
+            obj_dict[key] = value.to_dict()
+        st = json.dumps(obj_dict)
+        with open(self.__file_path, "w+") as f:
+            f.write(st)
+
+    def reload(self):
+        ''' deserializes the JSON file to __objects '''
+        try:
+            with open(self.__file_path, "r") as f:
+                obj_dict = json.loads(f.read())
+                for key, value in obj_dict.items():
+                    self.__objects[key] = bm.BaseModel(**value)
+        except FileNotFoundError:
+            pass
