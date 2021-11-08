a01048d5b711fb4138de286ef88ba98b8b7cd0c3
Ayrton Coelho <3282@holbertonschool.com>
2021-11-08 17:01:06 -0300

    console: add create command

--- console.py
+++ console.py
@@ -2,6 +2,11 @@
 '''
 '''
 import cmd
+import models
+from models.base_model import BaseModel
+
+dict_of_classes = {'BaseModel': BaseModel}
+
 
 class HBNBCommand(cmd.Cmd):
     '''
@@ -22,6 +27,22 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         return cmd.Cmd.emptyline(self)
+
+    def do_create(self, args):
+        try:
+            arguments = args.split()
+            if len(arguments) == 0:
+                print("** class name missing **")
+                return False
+            if arguments[0] in dict_of_classes:
+                obj = dict_of_classes[arguments[0]]()
+            else:
+                print("** class doesn't exist **")
+                return False
+            print(obj.id)
+            obj.save()
+        except Exception:
+            raise
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
