6afcf856f982ef8d214a18088985e359db771de4
GuzhiRegem <sebastian.moreiradl@gmail.com>
2021-11-09 10:34:09 -0800

    commit

--- console.py
+++ console.py
@@ -5,10 +5,37 @@ import cmd
 from models import storage
 from models.engine import classes
 
+def count_class(st):
+    args = st.split()
+    out = 0
+    for a in storage.all():
+        if a.startswith(args[0]):
+            out += 1
+    print(out)
 class HBNBCommand(cmd.Cmd):
     '''
     '''
     prompt = "(hbnb) "
+
+    def default(self, arg):
+        ''' default '''
+        fun_dict = {
+            "all":self.do_all,
+            "count":count_class,
+            "show":self.do_show,
+            "create":self.do_create
+        }
+        args = arg.split(".")
+        f_class = args[0]
+        if (f_class in classes) and len(args) >= 2:
+            args = args[1].split("(")
+            f_func = args[0]
+            if (f_func in fun_dict) and len(args) >= 2:
+                f_args = " ".join(tuple(args[1][:-1].split(", ")))
+                fun_dict[f_func]("{} {}".format(f_class, f_args))
+                return False
+        print("*** Unknown syntax: {}".format(arg))
+        return False
 
     def do_EOF(self, line):
         '''End of file, exits'''
@@ -113,8 +140,7 @@ class HBNBCommand(cmd.Cmd):
             print("** value missing **")
             return False
         setattr(storage.all()[key], arguments[2], arguments[3])
-
-
+    
 
 
 if __name__ == '__main__':
