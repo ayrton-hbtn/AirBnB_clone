bda405ef93ef59428f1073b8c222cde204c04814
hippocampus3282 <3282@holbertonschool.com>
2021-11-14 10:13:43 -0800

    file_storage: unittests in progress

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,20 @@
     return: nothing
 """
 import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+import json
+import pep8
 
 class Tester(unittest.TestCase):
-    """ Test class """
+    """ Test FileStorage class """
+    def test_docstring(self):
+        """ Test class methods docstrings """
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+        self.assertIsNotNone(FileStorage.delete.__doc__)
+
+    def test_isinstance(self):
